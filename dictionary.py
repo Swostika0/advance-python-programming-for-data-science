857260e3611621c813a1a4a95a95968cb2824688
swostika Bajagain <sostikabajgai8@gmail.com>
2026-03-27 12:40:07 +0000

    initial commit

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+student = {
+    "name": "swostika bajagain",
+    "age": 23,
+    "course": "python programming",
+    "marks": [85,90,95], # dict can include list as its element
+    "hobbies": ("dancing", "coding","eating","tavelling") # includes tuple as well
+    }
+print("my intro: ",student)
