5f316ba3bcc2215989cf0ddf773c6729e7e72d3f
swostika Bajagain <sostikabajgai8@gmail.com>
2026-03-29 07:59:05 +0000

    constructor example in python

---
+++ iteratorsAndGenerators/initfunction.py
@@ -0,0 +1,16 @@
+#actually __init__ is a constructor method
+class Student:
+    def __init__(self):
+        print("Object created")
+s1 = Student()
+print(s1)
+
+class Student:
+    def __init__(self, name,roll):
+        self.name = name
+        self.roll = roll
+
+s1 = Student("Pihu",4)
+print(s1.name)
+s2 = Student("swosti",16)
+print(f"I am {s2.name} and my age is {s2.roll}" )
