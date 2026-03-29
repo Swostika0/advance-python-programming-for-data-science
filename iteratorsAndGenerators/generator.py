673e8b2486cc7a919943b576e491d33adfedc127
swostika Bajagain <sostikabajgai8@gmail.com>
2026-03-29 08:41:08 +0000

    generators in python

---
+++ iteratorsAndGenerators/generator.py
@@ -0,0 +1,20 @@
+#generator are the function that returns iterator object using yield instead of return
+#example 1
+def count():
+    yield 1 #yield ia like an elevator stoping floor by floor
+    yield 2 #pause-->print-->resume
+    yield 3
+    yield 4
+    yield 100
+
+#example2
+for i in count():
+    print(i)
+#example2
+def count(n):
+    i = 1
+    while i <= n:
+        yield i 
+        i += 1
+for x in count(5):
+    print(x)
