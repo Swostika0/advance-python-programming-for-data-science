3c76053d834a5a270ce002df0143f4ab86306e0a
swostika Bajagain <sostikabajgai8@gmail.com>
2026-04-28 17:12:44 +0000

    simple array manipulation in list and need of numpy

---
+++ NumPY/list_array.py
@@ -0,0 +1,6 @@
+data = [50,50,47,97,49,3,53,42]
+print(data)
+print(data*2)
+
+"""here we can clearly see data repeating again when multiplied but we actually wanted to double it
+    this problem is soved by numpy that example is in another module"""
