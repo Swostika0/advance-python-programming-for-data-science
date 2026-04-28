27d451ab8574c1436326440562b933cdb1baee1b
swostika Bajagain <sostikabajgai8@gmail.com>
2026-04-28 06:01:41 +0000

    pass control statement in for loop

---
+++ control statement in python/pass.py
@@ -0,0 +1,5 @@
+for letter in 'Hi I love Travelling':
+    pass
+print('Last Letter :', letter)
+"""the loop iterates over each letter in 'Hi I love Travelling' but doesn't perform any operation,
+ and after the loop finishes, the last letter ('g') is printed."""
