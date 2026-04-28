daae641d7ef3b8ace12e0f8cbd846c1fd8f4bd5c
swostika Bajagain <sostikabajgai8@gmail.com>
2026-04-28 17:18:41 +0000

    array manipulation using numpy

---
+++ NumPY/array_manipulation.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+data = [50,50,47,97,49,3,53,42]
+grades  = np.array(data)
+print(grades)
+print(grades * 2)
+
+"""
+now the outputs are
+[50 50 47 97 49 3 53 42] (without comma)
+[100 100 .....] (as we expected double of the data)
+hence array manipulation is advanced in numpy
+"""
