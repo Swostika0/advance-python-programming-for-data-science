69d95a2f108597c6e616292e52e18f14acb28d63
swostika Bajagain <sostikabajgai8@gmail.com>
2026-04-28 05:54:33 +0000

    break statement

---
+++ control statement in python/break.py
@@ -0,0 +1,8 @@
+for letter in 'Hi I love Travelling':
+    if letter == 'e' or letter =='T':
+        break
+print('Current Letter :', letter)
+
+"""loop starts from the first character 'H' and continues until it encounters 'e'.
+ When 'e' is found, the break statement stops the loop immediately.
+ Since 'e' is the value at which the loop stops, it is printed outside the loop."""
